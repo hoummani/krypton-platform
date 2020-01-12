6e6275ccd3764886c7885c4829139f8819a0da19
hoummani <yassinehoummani.tsi@gmail.com>
2020-01-13 00:16:51 +0100

    adding some tests

---
+++ src/containers/dashboard/Account.test.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Account from './Accounts';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<Account />', () => {
+  let wrapper;
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, 'useState');
+  useStateSpy.mockImplementation(init => [init, setState]);
+
+  beforeEach(() => {
+    wrapper = Enzyme.mount(Enzyme.shallow(<Account />).get(0));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
+
+describe('Title input', () => {
+  it('Should capture title correctly onChange', () => {
+    const title = wrapper.find('input').at(0);
+    title.instance().value = 'Test';
+    title.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Test');
+  });
+});
+
+describe('Content input', () => {
+  it('Should capture content correctly onChange', () => {
+    const content = wrapper.find('input').at(1);
+    content.instance().value = 'Testing';
+    content.simulate('change');
+    expect(setState).toHaveBeenCalledWith('Testing');
+  });
+});
